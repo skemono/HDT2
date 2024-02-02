7a7c1586890b218ebba378d0b8c774ba4f1ba1e7
Zacatac23 <132374201+Zacatac23@users.noreply.github.com>
2024-02-01 19:31:07 -0600

    pruebas unitarias para la implementación de la pila

---
+++ src/main/java/com/nashe/TestPila.java
@@ -0,0 +1,44 @@
+package com.nashe;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPila {
+
+    private Stack<Integer> pila;
+
+    @Before
+    public void setUp() {
+        pila = new StackArrayList<>();
+    }
+
+    @Test
+    public void testPushPop() {
+        pila.push(1);
+        pila.push(2);
+
+        assertEquals(2, (int) pila.pop());
+        assertEquals(1, (int) pila.pop());
+    }
+
+    @Test
+    public void testPeek() {
+        pila.push(3);
+        pila.push(5);
+
+        assertEquals(5, (int) pila.peek());
+        assertEquals(2, pila.size()); // La pila no debe cambiar después de un peek
+    }
+
+    @Test
+    public void testEmpty() {
+        assertTrue(pila.empty());
+
+        pila.push(7);
+        assertFalse(pila.empty());
+
+        pila.pop();
+        assertTrue(pila.empty());
+    }
+}
